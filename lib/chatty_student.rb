2a96c9db4ad193ade4b979a89c8c62273a3b0db1
boruch d <dubywider@gmail.com>
2019-06-07 16:51:32 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,8 +1,8 @@
 class ChattyStudent < Student
   def hello
     super
-    puts"Hey there! I'm so excited to learn stuff.\nHoware you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
-  end 
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
   def raise_hand 
     super 
     10.times do
