265c3b927aa76273084debab7d8c2afa109874b4
boruch d <dubywider@gmail.com>
2019-06-07 16:39:37 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,3 +1,8 @@
-class ChattyStudent 
-  
+class ChattyStudent < Student
+  def hello
+    puts "Hey there! I'm so excited to learn stuff"
+  end 
+  def raise_hand 
+    puts "returns the phrase 'Pick me!'"
+  end  
 end 
