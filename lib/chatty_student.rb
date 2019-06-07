d011847c1a26d6f4b68adfd11478f9f482f4291b
boruch d <dubywider@gmail.com>
2019-06-07 16:45:56 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,7 @@
 class ChattyStudent < Student
   def hello
     super
-    puts ""Hey there! I'm so excited to learn stuff.\nHoware you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    puts"Hey there! I'm so excited to learn stuff.\nHoware you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end 
   def raise_hand 
     super 
